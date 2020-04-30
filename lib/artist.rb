f69bcf0854fe2eb3650ee520dab9e536e2fc5d95
Danielle Ellis <thedanielleellis@gmail.com>
2020-04-30 05:38:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,3 @@
-class Artist 
- attr_accessor :name 
- 
- def initialize (name) 
-   @name = name
- end 
- 
- def songs
-  Song.all.select{|song|song.artist==self} 
- end 
- 
- def add_song(song)
-    song.artist = self 
-  end 
- 
-  def add_song_by_name(name)
-    song = Song.new(name) 
-    song.artist = self  
-  end
-  
-  def self.song_count
-  Song.all.select{|song|song.artist}.count()
-  end 
-end 
-
-
 class Artist
   attr_accessor :name
 
