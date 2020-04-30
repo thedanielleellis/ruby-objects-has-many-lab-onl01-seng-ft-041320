5742a58c82ee6313d36e5da2bae0b1c1f0e7f641
Danielle Ellis <thedanielleellis@gmail.com>
2020-04-30 05:39:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,18 @@
-class Song 
+class Song
   attr_accessor :name, :artist
+
   @@all = []
-  
+
   def initialize(name)
     @name = name
-    save
+    @@all << self
   end
-  
-  def save 
-  @@all << self 
-  end 
-  
-  def self.all 
-    @@all 
-  end 
-end 
 
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    artist.name if artist
+  end
+end
