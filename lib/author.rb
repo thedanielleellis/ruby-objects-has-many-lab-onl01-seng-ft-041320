fcb33097d0c74d8d3632426313fa86c836ef8e1a
Danielle Ellis <thedanielleellis@gmail.com>
2020-04-30 05:23:31 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,11 @@
+class Author 
+  attr_accessor :name
+  
+  def initialize (name) 
+    @name = name 
+  end 
+  
+  def posts 
+    @posts = posts
+  end
+end 
---
+++ lib/post.rb
@@ -0,0 +1,17 @@
+class Post 
+ attr_accessor :title, :author
+  @@all = []
+    
+  def initialize (title)
+    @title = title 
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all
+  end 
+end 
+
+
+  # def author_name
+  #  author.name if author
